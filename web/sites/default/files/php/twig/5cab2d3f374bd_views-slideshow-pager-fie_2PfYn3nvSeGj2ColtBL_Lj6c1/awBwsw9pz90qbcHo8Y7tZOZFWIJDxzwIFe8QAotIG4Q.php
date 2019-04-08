fc5f0235e257fd4dda8d5070a30ab0985e770735
Eneko Trabudua <eneko.trabudua@biko2.com>
2019-04-08 13:30:54 +0200

    Añadido Slide de noticias en la HomePage

---
+++ web/sites/default/files/php/twig/5cab2d3f374bd_views-slideshow-pager-fie_2PfYn3nvSeGj2ColtBL_Lj6c1/awBwsw9pz90qbcHo8Y7tZOZFWIJDxzwIFe8QAotIG4Q.php
@@ -0,0 +1,112 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/views_slideshow/templates/views-slideshow-pager-field-field.html.twig */
+class __TwigTemplate_b1772c8ab237e680fb0ed86d2c3e2cc9b147d84bb989ca8efed8f2aa9f0cc6f2 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 17];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 16
+        echo "<div class=\"views-field-";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["css_identifier"] ?? null)), "html", null, true);
+        echo "\">
+  ";
+        // line 17
+        if (($context["label"] ?? null)) {
+            // line 18
+            echo "    <label class=\"view-label-";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["css_identifier"] ?? null)), "html", null, true);
+            echo "\">
+      ";
+            // line 19
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null)), "html", null, true);
+            echo ":
+    </label>
+  ";
+        }
+        // line 22
+        echo "  <div class=\"views-content-";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["css_identifier"] ?? null)), "html", null, true);
+        echo "\">
+    ";
+        // line 23
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["output"] ?? null)), "html", null, true);
+        echo "
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/views_slideshow/templates/views-slideshow-pager-field-field.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  78 => 23,  73 => 22,  67 => 19,  62 => 18,  60 => 17,  55 => 16,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/views_slideshow/templates/views-slideshow-pager-field-field.html.twig", "/var/www/html/web/modules/contrib/views_slideshow/templates/views-slideshow-pager-field-field.html.twig");
+    }
+}
